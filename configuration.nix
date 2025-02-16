de92776b52c81fb91ed9812d2b8d7e105cb7a633
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2025-02-16 19:22:40 -0300

    Docker enable

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,4 @@
-{ pkgs
-, config
-, ...
-}:
+{ pkgs, config, ... }:
 let
   domain = "pbcarrara.com.br";
   email = "piticarrara@gmail.com";
@@ -9,18 +6,14 @@ let
   adguardPort = 3333;
 
   env = import ./env.nix;
-in
-{
-  imports = [
-    ./hardware-configuration.nix
-  ];
+in {
+  imports = [ ./hardware-configuration.nix ];
 
   nix.gc = {
     automatic = true;
     randomizedDelaySec = "14m";
     options = "--delete-older-than 1d";
   };
-
 
   programs = {
     git = {
@@ -31,6 +24,8 @@ in
       };
     };
   };
+
+  virtualisation.docker.enable = true;
 
   security.acme.acceptTerms = true;
   security.acme.defaults.email = email;
@@ -58,10 +53,8 @@ in
         locations."/" = {
           proxyPass = "http://127.0.0.1:${toString trilliumPort}";
           proxyWebsockets = true;
-          extraConfig =
-            "proxy_ssl_server_name on;" +
-            "proxy_pass_header Authorization;"
-          ;
+          extraConfig = "proxy_ssl_server_name on;"
+            + "proxy_pass_header Authorization;";
         };
       };
       "adguard.${domain}" = {
@@ -70,10 +63,8 @@ in
         locations."/" = {
           proxyPass = "http://127.0.0.1:${toString adguardPort}";
           proxyWebsockets = true;
-          extraConfig =
-            "proxy_ssl_server_name on;" +
-            "proxy_pass_header Authorization;"
-          ;
+          extraConfig = "proxy_ssl_server_name on;"
+            + "proxy_pass_header Authorization;";
         };
       };
     };
@@ -96,9 +87,7 @@ in
     port = adguardPort;
   };
 
-  networking = {
-    firewall.enable = false;
-  };
+  networking = { firewall.enable = false; };
 
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = false;
@@ -106,8 +95,8 @@ in
   networking.domain = "";
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
-    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOi+CZumNvuUVU+TPJ26CDdYAzKkaQ2YJ9EnFa172VRO pbcarrara@inf.ufrgs.br''
-    ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwdtnVDxmDeNFM5YH4wsa1k9qTLlYcbAuqXtZUbwkZdLzvfdkMav8OH2mMra9lkEjBKgjniGmvCaGYXyPINVD4a7J9UKlaNVExXJ4PzVMbM/3Kvjo0WcMKh/mQKeLmF6aYBIOjyO0V4JsbiSb9N4lleD9mPD5NSJSCphZuMYzuSILj7C9X46/0bQSqW3vp61YAWzetdNnyYwTFVnibr2w2U7WRIXFDUvJX5oRSvI2QLn1bn9J5zaRbT6FwgIIfeliAI4rrwIWmLhfeAOz8X+dTwaC/Jb3cFXf4CtQhO0+aLzR236q4nqAheW7xhSidzavhq12kFGRmupMbCj9skCf7m0HFkwPABuw0AY/j5DwVTTuhuAgoYyjKlFxbX7vm+3puqJNh9DO2S0DHetEQykhAP+PRiHLpWZXKQ5+ZdVHQatGOiJxuSTUs3yBQPy0V4vA9sQu6fsB1x1l658/rv+fIy9DYgFHpAfsjyH5iEIJKwwzMCSAbuTLPi7zRoyTdsR8= pietro@hope''
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOi+CZumNvuUVU+TPJ26CDdYAzKkaQ2YJ9EnFa172VRO pbcarrara@inf.ufrgs.br"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwdtnVDxmDeNFM5YH4wsa1k9qTLlYcbAuqXtZUbwkZdLzvfdkMav8OH2mMra9lkEjBKgjniGmvCaGYXyPINVD4a7J9UKlaNVExXJ4PzVMbM/3Kvjo0WcMKh/mQKeLmF6aYBIOjyO0V4JsbiSb9N4lleD9mPD5NSJSCphZuMYzuSILj7C9X46/0bQSqW3vp61YAWzetdNnyYwTFVnibr2w2U7WRIXFDUvJX5oRSvI2QLn1bn9J5zaRbT6FwgIIfeliAI4rrwIWmLhfeAOz8X+dTwaC/Jb3cFXf4CtQhO0+aLzR236q4nqAheW7xhSidzavhq12kFGRmupMbCj9skCf7m0HFkwPABuw0AY/j5DwVTTuhuAgoYyjKlFxbX7vm+3puqJNh9DO2S0DHetEQykhAP+PRiHLpWZXKQ5+ZdVHQatGOiJxuSTUs3yBQPy0V4vA9sQu6fsB1x1l658/rv+fIy9DYgFHpAfsjyH5iEIJKwwzMCSAbuTLPi7zRoyTdsR8= pietro@hope"
   ];
 
   system.stateVersion = "23.11";
